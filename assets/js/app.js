3de88e7cf09289d0fbd068677ebcf6b628169862
Musharrof Shishir <musharrofshishir@users.noreply.github.com>
2020-05-05 01:40:54 +0600

    Updated js file and logic
    update js file with limit logics.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,57 @@
+let counter = document.querySelector("#counter").value; //counter showing in the browser
+let limit = 0;  //INITIALIZING LIMIT VALUE
+let minLimit = 0;   //FOR NEGETIVE VALUE CONVERSION
+let count = parseInt(counter);  //converting from string to number/integer
+//FUNCTION FOR SETTING UPA A MAXIMUM AND A MINIMUM VALUE    
+const limited = () => {
+    let lim = document.querySelector("#limit").value;
 
-let counter = document.querySelector("#counter").value;
+    limit = parseInt(lim);
 
-let count = parseInt(counter);
-    
+    minLimit = -(limit); // FOR NEGETIVE NUMBER
 
+    return limit;
+}
+//FUNCTION FOR COUNTING UP/ INCREAMENT
 const inc = () => {
+    // LOGIC TO DETERMINE MAXIMUM AND MINIMUM VALUE AND THEN COUNTING UP
+    if(limit !== 0 ){
+        if((limit <= count) && (minLimit <= count) ){
+            alert("YOU HAVE REACHED THE LIMIT !!!"); // alert() FUNCTION TO ALERT THAT YOU HAVE REACHED THE MAXIMUM OR MINIMUM VALUE/LIMIT
+        }
+        else{
+            // COUNTING UP LOGIC
+            count += 1;            
+            counter = count;
+            //PASSING THE NEW VALUE TO THE DOM
+            document.querySelector("#counter").value = counter;
+        }
+    }
+    else{
     count += 1;
-
     counter = count;
-
     document.querySelector("#counter").value = counter;
+    };
 }
-
+//FUNCTION FOR COUNTING DOWN/ DECREAMENT
 const dec = () => {
+    if(limit !== 0 ){
+        if((limit >= count) && (minLimit >= count) ){
+                alert("YOU HAVE REACHED THE LIMIT !!!");
+        }
+        else{
+            // COUNTING DOWN LOGIC
+            count -= 1;
+            counter = count;
+            //PASSING THE NEW VALUE TO THE DOM
+            document.querySelector("#counter").value = counter;
+        }
+    }
+    else{
     count -= 1;
-
     counter = count;
-
     document.querySelector("#counter").value = counter;
+    };
 }
+//END
+
